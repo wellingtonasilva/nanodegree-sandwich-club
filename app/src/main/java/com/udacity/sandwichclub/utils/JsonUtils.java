a3d0f6281db65563da3723401afd5523d1dd2fb7
Wellington A. Silva <wellingtonasilva@gmail.com>
2020-04-12 12:38:42 +0100

    fix: Apply suggestions from Reviewer
    Code review suggestions:
    
    - Remove FrameLayout from Detail layout
    - Create/use dimension instead of hard coded value
    - Create/use constant string when get value from JSON converter
    - Change getString for optString
    - Using placeholder and error in Picasso
    - Using android:parentActivityName

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -11,17 +11,25 @@ import java.util.List;
 
 public class JsonUtils {
 
+    private static final String NAME = "name";
+    private static final String MAIN_NAME = "mainName";
+    private static final String ALSO_KNOWN_AS = "alsoKnownAs";
+    private static final String PLACE_OF_ORIGIN = "placeOfOrigin";
+    private static final String DESCRIPTION = "description";
+    private static final String IMAGE = "image";
+    private static final String INGREDIENTS = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
         try {
             JSONObject jsonObject = new JSONObject(json);
-            JSONObject name = jsonObject.getJSONObject("name");
+            JSONObject name = jsonObject.optJSONObject(NAME);
 
-            return new Sandwich(name.getString("mainName"),
-                    from(name.getJSONArray("alsoKnownAs")),
-                    jsonObject.getString("placeOfOrigin"),
-                    jsonObject.getString("description"),
-                    jsonObject.getString("image"),
-                    from(jsonObject.getJSONArray("ingredients")));
+            return new Sandwich(name.optString(MAIN_NAME),
+                    from(name.optJSONArray(ALSO_KNOWN_AS)),
+                    jsonObject.optString(PLACE_OF_ORIGIN),
+                    jsonObject.optString(DESCRIPTION),
+                    jsonObject.optString(IMAGE),
+                    from(jsonObject.optJSONArray(INGREDIENTS)));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -34,10 +42,10 @@ public class JsonUtils {
      * @return List of String
      * @throws JSONException
      */
-    private static List<String> from(JSONArray jsonArray) throws JSONException {
+    private static List<String> from(JSONArray jsonArray) {
         List<String> result = new ArrayList<>();
         for (int i = 0; i < jsonArray.length(); i++) {
-            result.add(jsonArray.getString(i));
+            result.add(jsonArray.optString(i));
         }
         return result;
     }
